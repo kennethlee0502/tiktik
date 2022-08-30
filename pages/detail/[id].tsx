6a91ca206e000109dc32e574dff599d400e64f41
Kenneth Lee <95536900+kennethlee0502@users.noreply.github.com>
2022-08-30 15:11:37 -0400

    comment section done

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -24,6 +24,8 @@ const Detail = ({ postDetails }: IProps) => {
   const router = useRouter();
   const videoRef = useRef<HTMLVideoElement>(null);
   const { userProfile }: any = useAuthStore();
+  const [comment, setComment] = useState("");
+  const [isPostingComment, setIsPostingComment] = useState(false);
 
   const onVideoClick = () => {
     if (playing) {
@@ -49,6 +51,21 @@ const Detail = ({ postDetails }: IProps) => {
         like,
       });
       setPost({ ...post, likes: data.likes });
+    }
+  };
+
+  const addComment = async (e) => {
+    e.preventDefault();
+
+    if (userProfile && comment) {
+      setIsPostingComment(true);
+      const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+        userId: userProfile._id,
+        comment,
+      });
+      setPost({ ...post, comments: data.comments });
+      setComment("");
+      setIsPostingComment(false);
     }
   };
 
@@ -137,7 +154,13 @@ const Detail = ({ postDetails }: IProps) => {
               />
             )}
           </div>
-          <Comments />
+          <Comments
+            comment={comment}
+            setComment={setComment}
+            addComment={addComment}
+            comments={post.comments}
+            isPostingComment={isPostingComment}
+          />
         </div>
       </div>
     </div>
