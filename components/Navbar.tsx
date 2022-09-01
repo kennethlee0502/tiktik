5d8658f5594406ca7e34ceb33fb0dda7803acf29
Kenneth Lee <95536900+kennethlee0502@users.noreply.github.com>
2022-09-01 16:17:41 -0400

    search

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -13,11 +13,20 @@ import Logo from "../utils/tiktik-logo.png";
 import { createOrGetUser } from "../utils/index.ts";
 
 const Navbar = () => {
+  const router = useRouter();
   const { userProfile, addUser, removeUser } = useAuthStore();
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
   return (
-    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4 ">
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div className="w-[100px] md:w-[130px]">
+        <div className="w-[100px] md:w-[129px] md:h-[30px] h-[38px]">
           <Image
             className="cursor-pointer"
             src={Logo}
@@ -26,7 +35,26 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div>SEARCH</div>
+      <div clasName="relative hidden md:block">
+        <form
+          onSubmit={handleSearch}
+          className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input
+            tyope="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search accounts and videos"
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0"
+          />
+          <button
+            onClick={handleSearch}
+            // className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
       <div>
         {userProfile ? (
           <div className="flex gap-5 md:gap-10">
@@ -53,7 +81,7 @@ const Navbar = () => {
             )}
             <button
               type="button"
-              className="px-2"
+              className=" border-2 p-2 rounded-full cursor-pointer outline-none shadow-md"
               onClick={() => {
                 googleLogout();
                 removeUser();
