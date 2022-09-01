39970057e87bd8aaf07d49353b4fd8d87468f864
Kenneth Lee <95536900+kennethlee0502@users.noreply.github.com>
2022-09-01 16:24:42 -0400

    deploy error

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { GoVerified } from "react-icons/go";
 
-import useAuthStore from "../store/authStore.ts";
+import useAuthStore from "../store/authStore";
 import NoResults from "./NoResult";
 import { IUser } from "../types.d.ts";
 
